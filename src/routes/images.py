601a35de20e97b221509af8dd139f7e2ddce8226
Evgenius87 <plotnikov.evgenij@ukr.net>
2023-07-28 19:55:29 +0300

    fix images2

--- src/routes/images.py
+++ src/routes/images.py
@@ -9,7 +9,7 @@ from src.services.users import auth_service
 
 from src.database.db_connection import get_db
 from src.database.models import User
-from src.schemas import ImageGetAllResponse, ImageUpdateDescrResponse, \
+from src.schemas import ImageGetAllResponse, ImageNameUpdateModel, ImageNameUpdateResponse, \
     ImageGetResponse, ImageAddTagResponse, ImageSignModel, ImageAddResponse, ImageDeleteResponse, ImageAddModel, ImageChangeSizeModel, ImageChangeColorModel, ImageTransformModel
 from src.repository import users as repository_users
 from src.repository import images as repository_images
@@ -24,37 +24,45 @@ router = APIRouter(prefix='/images', tags=["images"])
 security = HTTPBearer()
 
 
-@router.get('/qrcode/{image_id}'""", response_model=ImageResponse знайти модель для поверненя зображення""")
-async def get_qr_for_id(image_id: int,
-                        db: Session = Depends(get_db)):
-    pass
+
+@router.get("/{image_id}", response_model=ImageGetResponse)
+async def get_image(image_id: int, db: Session = Depends(get_db),
+                    current_user: User = Depends(auth_service.get_current_user)):
+    url = await repository_images.get_image(db, image_id)
+    return {'url': url}
 
 
 @router.get("", response_model=ImageGetAllResponse)
-async def get_images(db: Session = Depends(get_db)):
-    user_images = await repository_images.get_images(db)
-    return {"images": user_images}
+async def get_images(db: Session = Depends(get_db),
+                     current_user: User = Depends(auth_service.get_current_user)):
+    urls = await repository_images.get_images(db)
+    return {'urls': urls}
+
+@router.put('/{comment_id}', response_model=ImageNameUpdateResponse)
+async def update_image(body: ImageNameUpdateModel, db: Session = Depends(get_db),
+                         current_user: User = Depends(auth_service.get_current_user)):
+    db_image, detail = await repository_images.img_update_name(body, db, current_user)
+    return db_image, detail
 
 
-@router.get("/image_id/{id}", response_model=ImageGetResponse)
-async def get_image(id: int, db: Session = Depends(get_db)):
-    user_image, comments = await repository_images.get_image(db, id)
-    return {"image": user_image, "comments": comments}
-
-
-##########Тут має бути завантаження на Cloudinary
 @router.post('/', response_model=ImageAddResponse, status_code=status.HTTP_201_CREATED)
 async def create_new_images(file: UploadFile = File(),
-                            title: str = None,
+                            image_name: str = None,
                             description: str = None,
                             tags: str = None,
                             db: Session = Depends(get_db),
                             current_user: User = Depends(auth_service.get_current_user)):
     file.filename = f"{uuid.uuid4()}.jpg"
     contents =  await file.read()
-    url = await image_cloudinary.add_image(contents, title)
-    tags_list = [tag for tag in tags.split(',')]
-    db_image, detail = await repository_images.add_image(db=db, url=url, tags=tags_list, title=title, description=description, user=current_user)
+    url, public_id = await image_cloudinary.add_image(contents)
+    tags_list = tags.replace(",", "").replace(".", "").replace("/", "").split()
+    db_image, detail = await repository_images.add_image(db=db, 
+                                                         url=url, 
+                                                         tags=tags_list, 
+                                                         image_name=image_name, 
+                                                         public_id=public_id, 
+                                                         description=description, 
+                                                         user=current_user)
     return {"image": db_image, "details": detail}
 
 
@@ -75,7 +83,7 @@ async def change_size_image(body: ImageChangeSizeModel,
 
 
 @router.post('/change_color', response_model=ImageAddResponse, status_code=status.HTTP_201_CREATED)
-async def change_size_image(body: ImageChangeColorModel, 
+async def change_color_image(body: ImageChangeColorModel, 
                             db: Session = Depends(get_db), 
                             current_user: User = Depends(auth_service.get_current_user)):
     db_image, detail = await repository_images.change_color_object_in_image(body=body, db=db, user = current_user)
@@ -98,7 +106,7 @@ async def cut_face_in_image(body: ImageTransformModel,
 async def sign_image(body: ImageSignModel, 
                             db: Session = Depends(get_db), 
                             current_user: User = Depends(auth_service.get_current_user)):
-    db_image, detail = await repository_images.cut_face_in_image(body=body, db=db, user = current_user)
+    db_image, detail = await repository_images.sign_image(body=body, db=db, user = current_user)
     if db_image is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="image not found")
     return {"image": db_image, "details": detail}
