fea6f8ec70d78f168ab485fd6c718bfc5df62d48
OlehVakulchyk <4020405@gmail.com>
2023-07-29 21:34:48 +0300

    Update 9df648d2a241_new_images.py

--- migrations/versions/9df648d2a241_new_images.py
+++ migrations/versions/9df648d2a241_new_images.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '9df648d2a241'
-down_revision = '8f6bccb1ef10'
+down_revision = None
 branch_labels = None
 depends_on = None
 
